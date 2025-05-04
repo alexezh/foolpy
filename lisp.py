d1a5e10d992fe8d876aacacfa4feee124ac737cc
Alexandre Grigorovitch <alexezh@gmail.com>
2025-05-04 09:21:59 -0700

    more on lisp

--- lisp.py
+++ lisp.py
@@ -1,5 +1,7 @@
 from collections import ChainMap
 import operator as op
+from typing import Any, List
+from collections import defaultdict
 
 def standard_env():
     env = {
@@ -54,61 +56,110 @@ def atom(token):
 
 def eval_(x, env):
     if isinstance(x, str):
+      if x.startswith('"') and x.endswith('"'):
+        return x
+      else:
         return env[x]
     elif not isinstance(x, list):
         return x
-    elif x[0] == 'if':
-      if len(x) == 3:
-          _, test, conseq = x
-          test_result = eval_(test, env)
-          if test_result:
-              return eval_(conseq, env)
-          else:
-              return None  # no 'else', return None
-      elif len(x) == 4:
-          _, test, conseq, alt = x
-          test_result = eval_(test, env)
-          if test_result:
-              return eval_(conseq, env)
-          else:
-              return eval_(alt, env)
     elif x[0] == 'define':
         (_, var, expr) = x
         env[var] = eval_(expr, env)
     elif x[0] == 'lambda':
         (_, params, body) = x
-        def func(*args):
+        def func(env, *args):
             local_env = ChainMap(dict(zip(params, args)), env)
             return eval_(body, local_env)
         return func
     else:
         proc = eval_(x[0], env)
         args = [eval_(arg, env) for arg in x[1:]]
-        return proc(env, *args)
+        return proc(env, args)
 
 def exec_(code):
   tokens = tokenize(code)
   ast = parse(tokens)
   eval_(ast, global_env)
 
+def if_(env, args: List[Any]):
+  if len(args) == 3:
+      _, test, conseq = args
+      test_result = eval_(test, env)
+      if test_result:
+          return eval_(conseq, env)
+      else:
+          return None  # no 'else', return None
+  elif len(args) == 4:
+      _, test, conseq, alt = args
+      test_result = eval_(test, env)
+      if test_result:
+          return eval_(conseq, env)
+      else:
+          return eval_(alt, env)
+
 # returns function 
-def meta_match(env, obj):
+def mmatch(env, args: List[Any]):
+  # now we want to select match function
+  # and we have multiple options, so we want to run all of them forking output
+  # to do this, we can change if(x, t, e) monad ? 
+  #
+  # if should return enumerator of options, function which can be called many time
+  # and if will propagate to for-each which will fork execution
   return True
 
-def meta_action(env, obj):
+def maction(env, args: List[Any]):
   print("Hello World")
 
-def for_each(env, func, list):
-  for arg in list(env):
+def for_each(env, args: List[Any]):
+  func, lst = args
+  for arg in lst(env):
       func(env, arg)
 
 def objects(env):
   return [1, 2]
 
+def msolve(env, args):
+    func, crit = args
+    return crit(env)
+
+def more(env, args):
+    a1, a2 = args
+    return a1(env) > a2(env)
+
+def reduce(env, args):
+    lst, func = args
+    for elem in lst:
+        func(env, elem)
+
+def group(env, args):
+    lst, func = args
+    result = defaultdict(list)
+
+    for item in lst(env):
+        key = func(env, item)
+        result[key].append(item)
+    return dict(result)    
+
+def isnumber(env, args):
+    val = eval_(args[0], env)
+    return isinstance(val, (int, float))
+
+def len_(env, args):
+    return len(args[0])
+
+add_external("if", if_)
 add_external("for-each", for_each)
-add_external("mmatch", meta_match)
-add_external("maction", meta_action)
+add_external("mmatch", mmatch)
+add_external("maction", maction)
+add_external("msolve", msolve)
+add_external("more", more)
+add_external("group", group)
+add_external("reduce", reduce)
+add_external("isnumber", isnumber)
+add_external("len", len_)
+
 add_external("objects", objects)
+# add_external("count", meta_action)
 
 # Example usage
 # if __name__ == '__main__':
--- main.py
+++ main.py
@@ -11,6 +11,56 @@ exec_("""(for-each
   ) objects
 )""")
 
+# solve 2 + x - (2 + y) + x + 4
+#  group_subexp - map()
+#  simplify
+#  ungroup_subexp
+#    why would we open - to continue, so we need goal
+#    to compute with X, I either need to open subexp, or bring into
+#    we can just compute in parallel for couple steps and compare the results
+#    later we can "analyze" what happened and decide why. Such as that we want to cut x
+#  criteriea - smaller + !same + 
+# solve then looks as following
+#   second part is goal, we want to be number, or number + var
+#   but for this we need to know which vars we have, so group objects, lambda(x, x => "number")
+#   and then we check that each category used once
+# simplify
+result = exec_("""
+(msolve 
+  (lambda (o)
+    (if 
+      (mmatch o)
+      (maction o)
+    )
+  )
+  (lambda ()
+    (more
+      (reduce 
+        (group objects 
+          (lambda (o) 
+            (if 
+              (isnumber o)
+              "number"
+              o
+            )
+          )
+        )
+        (lambda (group)
+          (if
+            (more 
+              (len group)
+              1
+            )
+            1
+            0
+          )
+        )
+      )
+      0
+    )
+  )
+)""")
+
 initDevice()
 
 # trainWordEmbedding();
